2718ee7d639c9784629f59134a400fd3a2a2bba3
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-11-03 23:19:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+import { states } from "./state.js";
+
+const search = document.querySelector('input')
+const matchList = document.querySelector('.display')
+
+function searchState (stateInput){
+
+    let matches =  states.filter((state)=>{
+        let regex =  new RegExp(`^${stateInput}`, "gi");
+        return state.match(regex)
+    })
+
+    if (search.value === ""){
+        matches = ""
+    }
+
+    html(matches)
+    
+}
+
+
+function html (match){
+    if(match.length > 0){
+        let html =  match.map((item)=>{
+            return `<li>${item}</li>`
+        }).join("")
+
+        matchList.innerHTML = html
+    }
+}
+
+search.addEventListener('input', ()=>{
+    if(search.value === ''){
+        matchList.innerHTML = ''
+    }
+    searchState(search.value)
+})
+
